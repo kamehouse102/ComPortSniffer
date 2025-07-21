d0b900cef558a3f83846be7ef16635cda3dc1e94
kame102 <kamehouse102@gmail.com>
2025-07-21 12:21:42 +0900

    Add files via upload

---
+++ ComPortSniffer.ps1
@@ -0,0 +1,230 @@
+﻿<################################################# COMポートスニファ Ver1.2 2021/02/27
+●スクリプト説明
+2つのCOMポートを使用してバケツリレーのように双方向にデータを中継することで、通信をモニターするスクリプトです。
+
+バッチまたは、ショートカットでの起動方法
+  powershell -WindowStyle Hidden -ExecutionPolicy RemoteSigned -command ".\スクリプトファイル名"  
+
+●使い方
+シリアル通信パラメータ設定を設定し、実行するとデータがコンソールに表示されます。
+起動オプションでCOMポートのパラメータを指定可能。
+
+例）COMポートスニファ.exe COM1 19200 None 8 One COM2 38400 None 8 One -byte -log
+
+●変更履歴
+V1.0  初版
+V1.1  起動オプション設定を追加
+V1.2  CTRL＋Cで入力でlog出力して終了を追加
+
+#>
+#---+---+---+---+---+---+---+---+---+---+ 起動オプション パラメータ取得
+Param(
+    [string]$Arg1 = "COM1",    #COM番号
+    [string]$Arg2 = "38400",   #bps
+    [string]$Arg3 = "None",    #パリティ
+    [string]$Arg4 = "8",       #データ長
+    [string]$Arg5 = "One",     #ストップビット
+
+    [string]$Arg6 = "COM2",    #COM番号
+    [string]$Arg7 = "38400",   #bps
+    [string]$Arg8 = "None",    #パリティ
+    [string]$Arg9 = "8",       #データ長
+    [string]$ArgA = "One",     #ストップビット
+
+    [Switch]$Byte = $false,    #バイト値表示する
+    [Switch]$Log = $false      #log出力する
+)
+
+Set-StrictMode -Version Latest    #コーディング規則を設定して適用します。（デバッグに有用）
+
+################################################## 初期設定
+#---+---+---+---+---+---+---+---+---+---+ 変数宣言
+# シリアル通信パラメータ設定
+$ComParam1 = $Arg1, $Arg2, $Arg3, $Arg4, $Arg5    #1番目 COMポート設定 COM番号、ボーレート、データ長、パリティ、ストップ
+$ComParam2 = $Arg6, $Arg7, $Arg8, $Arg9, $ArgA    #2番目 COMポート設定 COM番号、ボーレート、データ長、パリティ、ストップ
+
+#---+---+---+---+---+---+---+---+---+---+ エラー発生時動作
+$ErrorActionPreference = "stop"    #エラー発生時にスクリプトを中断します。
+
+################################################## 関数定義
+#---+---+---+---+---+---+---+---+---+---+ シリアル通信の制御線確認
+Function CtsDsrChk {
+    While (-not ($ComPortObj.CtsHolding -and $ComPortObj.DsrHolding)) {
+        $Input = Read-Host "機器の電源および接続を確認してください。"
+    }
+}
+#---+---+---+---+---+---+---+---+---+---+ 1番目 COM送信処理
+# 送信
+Function Send1($Str) {
+        $ComPortObj1.Write($Str)
+}
+
+#---+---+---+---+---+---+---+---+---+---+ 2番目 COM送信処理
+# 送信
+Function Send2($Str) {
+        $ComPortObj2.Write($Str)
+}
+
+#---+---+---+---+---+---+---+---+---+---+ 文字列 ⇒ バイト表示
+Function Bytes($Str) {
+        $Ary = [system.text.encoding]::Default.GetBytes("$Str")
+        $Byte_Str = ""
+        foreach ( $B in $Ary ){
+            $Byte_Str += [System.Convert]::ToString($B, 16) + " "    #16進数変換
+        }
+        Write-Host " $Byte_Str"
+}
+
+################################################## イニシャル処理
+#---+---+---+---+---+---+---+---+---+---+ 1番目 COMポート
+# COMポート生成、パラメータ設定
+$ComPortObj1 = New-Object System.IO.Ports.SerialPort $ComParam1    #COM番号、ボーレート、データ長、パリティ、ストップ
+$ComPortObj1.DtrEnable = $true    #DTR設定
+$ComPortObj1.RtsEnable = $true    #RTS設定
+$ComPortObj1.Handshake = "None"   # ハンドシェイク設定 (None、XOnXOff、RequestToSend、RequestToSendXOnXOff)
+$ComPortObj1.NewLine = "`r"       #改行文字設定（WriteLineやReadLineメソッドに適用）
+$ComPortObj1.Encoding=[System.Text.Encoding]::GetEncoding("Shift_JIS")    # 文字コード設定
+# シリアルポート受信イベント
+$ComRecvEventObj1 = Register-ObjectEvent -InputObject $ComPortObj1 -EventName "DataReceived" `
+    -Action {Param([System.IO.Ports.SerialPort]$sender, [System.EventArgs]$e) Write-Output $sender.ReadExisting()}
+# シリアルポートエラーイベント（エラー発生時に$trueを返す）通信でパリティエラーが発生した場合など
+$ComErrEventObj1 = Register-ObjectEvent -InputObject $ComPortObj1 -EventName "ErrorReceived" -Action {$true}
+# シリアルポート非データ信号イベント（制御ピンが変化した場合に$trueを返す）通信側との接続に異常がある場合など？
+#$ComPinEventObj = Register-ObjectEvent -InputObject $ComPortObj -EventName "PinChanged" -Action {$true}
+
+#---+---+---+---+---+---+---+---+---+---+ 2番目 COMポート
+# COMポート生成、パラメータ設定
+$ComPortObj2 = New-Object System.IO.Ports.SerialPort $ComParam2    #COM番号、ボーレート、データ長、パリティ、ストップ
+$ComPortObj2.DtrEnable = $true    #DTR設定
+$ComPortObj2.RtsEnable = $true    #RTS設定
+$ComPortObj2.Handshake = "None"   # ハンドシェイク設定 (None、XOnXOff、RequestToSend、RequestToSendXOnXOff)
+$ComPortObj2.NewLine = "`r"       #改行文字設定（WriteLineやReadLineメソッドに適用）
+$ComPortObj2.Encoding=[System.Text.Encoding]::GetEncoding("Shift_JIS")    # 文字コード設定
+# シリアルポート受信イベント
+$ComRecvEventObj2 = Register-ObjectEvent -InputObject $ComPortObj2 -EventName "DataReceived" `
+    -Action {Param([System.IO.Ports.SerialPort]$sender, [System.EventArgs]$e) Write-Output $sender.ReadExisting()}
+# シリアルポートエラーイベント（エラー発生時に$trueを返す）通信でパリティエラーが発生した場合など
+$ComErrEventObj2 = Register-ObjectEvent -InputObject $ComPortObj2 -EventName "ErrorReceived" -Action {$true}
+# シリアルポート非データ信号イベント（制御ピンが変化した場合に$trueを返す）通信側との接続に異常がある場合など？
+#$ComPinEventObj = Register-ObjectEvent -InputObject $ComPortObj -EventName "PinChanged" -Action {$true}
+
+#---+---+---+---+---+---+---+---+---+---+ COMポートオープン
+$ErrorActionPreference = "SilentlyContinue"    #エラー発生時に無視します。
+try {
+    $ComPortObj1.Open()
+} catch {
+    $Input = Read-Host "$ComParam1 ポートのオープンが出来ません。"
+    Exit    #スクリプト終了
+}
+try {
+    $ComPortObj2.Open()
+} catch {
+    $Input = Read-Host "$ComParam2 ポートのオープンが出来ません。"
+    $ComPortObj1.Close()
+    Exit    #スクリプト終了
+}
+$ErrorActionPreference = "stop"    #エラー発生時にスクリプトを中断します。
+
+#---+---+---+---+---+---+---+---+---+---+ logファイル出力
+$now = Get-Date -Format 'yyMMdd-HHmmss'    #現在日時
+$logname = "log_$now.txt"    #logファイル名
+if($Log) { Start-Transcript $logname }    #log記録開始
+
+################################################## メイン処理
+Write-Host "CTRL+C を入力すると終了"
+Try {
+    While($true) {
+        #1番目 COM受信確認
+        $RecvStr1 = Receive-job -job $ComRecvEventObj1
+        if($RecvStr1) {
+            Write-Host ">$RecvStr1"
+            if($Byte) { Bytes $RecvStr1 }
+            Send2 $RecvStr1
+        }
+
+        #2番目 COM受信確認
+        $RecvStr2 = Receive-job -job $ComRecvEventObj2
+        if($RecvStr2) {
+            Write-Host "<$RecvStr2"
+            if($Byte) { Bytes $RecvStr2 }
+            Send1 $RecvStr2
+        }
+
+        #通信エラー確認
+        If (Receive-job -job $ComErrEventObj1) { Write-Host "通信エラーが発生しました。" }
+        If (Receive-job -job $ComErrEventObj2) { Write-Host "通信エラーが発生しました。" }
+        #If (Receive-job -job $ComPinEventObj1) { Write-Host "接続エラー" }
+        #If (Receive-job -job $ComPinEventObj2) { Write-Host "接続エラー" }
+    }
+}
+################################################## 終了処理
+finally {
+    #---+---+---+---+---+---+---+---+---+---+ COMポートクローズ
+    $ComPortObj1.Close()
+    $ComPortObj2.Close()
+
+    if($Log) { Stop-Transcript }    #logファイル出力
+
+    Exit    #スクリプト終了
+}
+################################################## End Of File
+
+#シリアル通信のエラー(パリティエラーとか)については、ErrorReceivedイベント処理を追加すれば対応できる。
+#シリアル通信の制御信号については、PinChangedイベント処理を追加すれば対応できる。
+
+<#
+フィールド 
+InfiniteTimeout     タイムアウトが発生しないことを示します。
+
+プロパティ 
+BaudRate            シリアル ボー レートを取得または設定します。
+BreakState          ブレーク シグナルの状態を取得または設定します。
+BytesToRead         受信バッファー内のデータのバイト数を取得します。
+BytesToWrite        送信バッファー内のデータのバイト数を取得します。
+CDHolding           ポートのキャリア検出ラインの状態を取得します。
+CtsHolding          Clear To Send ラインの状態を取得します。
+DataBits            バイトごとのデータ ビットの標準の長さを取得または設定します。
+DiscardNull         ポートと受信バッファー間での送信時に、null バイトを無視するかどうかを示す値を取得または設定します。
+DsrHolding          DSR (Data Set Ready) シグナルの状態を取得します。
+DtrEnable           シリアル通信中に、DTR (Data Terminal Ready) シグナルを有効にする値を取得または設定します。
+Encoding            テキストの伝送前変換と伝送後変換のバイト エンコーディングを取得または設定します。
+Handshake           Handshake からの値を使用したデータのシリアル ポート伝送のハンドシェイク プロトコルを取得または設定します。
+IsOpen              SerialPort  オブジェクトの開いている状態または閉じた状態を示す値を取得します。
+NewLine             ReadLine() メソッドと WriteLine(String) メソッドの呼び出しの末尾を解釈する際に使用する値を取得または設定します。
+Parity             パリティ チェック プロトコルを取得または設定します。
+ParityReplace      パリティ エラーの発生時に、データ ストリーム内の無効なバイトを置き換えるバイトを取得または設定します。
+PortName           通信用のポートを取得または設定します。このポートには、使用可能なすべての COM ポートが含まれますが、これに限定されるわけではありません。
+ReadBufferSize     SerialPort の入力バッファーのサイズを取得または設定します。
+ReadTimeout        読み取り操作が完了していないときに、タイムアウトになるまでのミリ秒数を取得または設定します。
+ReceivedBytesThreshold DataReceived イベントが発生する前の、内部入力バッファーのバイト数を取得または設定します。
+RtsEnable          シリアル通信中に、RTS (Request To Send) シグナルが有効かどうかを示す値を取得または設定します。
+StopBits           バイトごとのストップ ビットの標準の数を取得または設定します。
+WriteBufferSize    シリアル ポートの出力バッファーのサイズを取得または設定します。
+WriteTimeout       書き込み操作が完了していないときに、タイムアウトになるまでのミリ秒数を取得または設定します。
+
+メソッド 
+Close()            ポート接続を閉じ、IsOpen プロパティを false に設定し、内部 Stream オブジェクトを破棄します。
+DiscardInBuffer()  シリアル ドライバーの受信バッファーからデータを破棄します。
+DiscardOutBuffer() シリアル ドライバーの送信バッファーからデータを破棄します。
+Dispose(Boolean)   SerialPort によって使用されているアンマネージド リソースを解放し、オプションでマネージド リソースも解放します。
+GetPortNames()     現在のコンピューターのシリアル ポート名の配列を取得します。
+Open()             新しいシリアル ポート接続を開きます。
+Read(Byte[], Int32, Int32)    SerialPort の入力バッファーから複数のバイトを読み取り、読み取ったバイトを指定したオフセットでバイト配列に書き込みます。
+Read(Char[], Int32, Int32)    SerialPort の入力バッファーから複数の文字を読み取り、読み取った文字を指定したオフセットで文字配列に書き込みます。
+ReadByte()         SerialPort の入力バッファーから、同期で 1 バイトを読み取ります。
+ReadChar()         SerialPort の入力バッファーから、同期で 1 文字を読み取ります。
+ReadExisting()     ストリームと SerialPort オブジェクトの入力バッファーの両方で、エンコーディングに基づいて、即座に使用できるすべてのバイトを読み取ります。
+ReadLine()         入力バッファー内の NewLine 値まで読み取ります。
+ReadTo(String)     入力バッファー内の指定した value まで文字列を読み取ります。
+Write(Byte[], Int32, Int32)    バッファーのデータを使用して、指定したバイト数をシリアル ポートに書き込みます。
+Write(Char[], Int32, Int32)    バッファーのデータを使用して、指定した文字数をシリアル ポートに書き込みます。
+Write(String)      指定した文字列をシリアル ポートに書き込みます。
+WriteLine(String)  指定した文字列と NewLine 値を出力バッファーに書き込みます。
+
+イベント
+メインスレッドではなくセカンダリスレッドで発生するため、メインスレッドの一部の要素を変更しようとすると、
+スレッド例外が発生する可能性があります。要素を変更する必要がある場合は、Invokeを使用して変更要求を戻します。
+DataReceived       SerialPort オブジェクトによって表されるポートを介してデータが受信されたことを示します。
+ErrorReceived      SerialPort オブジェクトによって表されるポートでエラーが発生したことを示します。
+PinChanged         非データ信号イベントが SerialPort オブジェクトによって表されるポートで発生したことを示します。
+#>
---
+++ ComPortSniffer_起動フォーム.ps1
@@ -0,0 +1,325 @@
+﻿<################################################# ComPortSniffer起動フォーム Ver1.1 2025/07/21
+●スクリプト説明
+・プログラムを起動する際に、オプションパラメータを簡単に選択して起動できるフォームです。
+
+●使い方
+・変更したい設定項目を入力すると起動オプションの欄に文字列が表示されます。
+・実行ボタンを押すと表示された起動オプションのパラメータを使用して、プログラムを起動します。
+・起動オプション欄のパラメータは、文字列をコピーしてバッチやショートカットのパラメータとして使用できます。
+#>
+
+################################################## 初期設定
+#---+---+---+---+---+---+---+---+---+---+ コーディング規則を設定して適用します。（デバッグに有用）
+Set-StrictMode -Version Latest    #コーディング規則を設定して適用します。（デバッグに有用）
+
+#---+---+---+---+---+---+---+---+---+---+ 変数宣言
+$WinTitle = "ComPortSniffer起動フォーム"  #ウィンドウタイトル
+$ScrName = ".\ComPortSniffer.exe"    #起動するファイル
+
+################################################## イニシャル処理
+#---+---+---+---+---+---+---+---+---+---+ アセンブリのロード
+Add-Type -AssemblyName System.Windows.Forms
+
+#---+---+---+---+---+---+---+---+---+---+
+Set-Location -Path $PSScriptRoot    #カレントディレクトリをスクリプトのディレクトリへ変更
+
+#---+---+---+---+---+---+---+---+---+---+ タイマー（起動オプション テキスト表示）
+$Timer = New-Object System.Windows.Forms.Timer
+$Timer.Interval = 1000    #イベントを発生させる間隔(ms)
+$Time = {
+    $TextStr = ""
+    #処理
+    If ($Combo_Com.Text -ne "") { $TextStr += " " + $Combo_Com.Text + " " + $Combo_Baud.Text + " " + $Combo_Parity.Text + " " + $Combo_Bit.Text + " " + $Combo_Stop.Text }
+    If ($Combo_Com2.Text -ne "") { $TextStr += " " + $Combo_Com2.Text + " " + $Combo_Baud2.Text + " " + $Combo_Parity2.Text + " " + $Combo_Bit2.Text + " " + $Combo_Stop2.Text }
+    If ($CheckBox_Bin.Checked) { $TextStr += " -byte" }
+    If ($CheckBox_Log.Checked) { $TextStr += " -log" }
+    $TextBox_Arg.Text = $TextStr
+}
+$Timer.Add_Tick($Time)
+
+#---+---+---+---+---+---+---+---+---+---+ メインフォーム
+#フォーム作成
+$Form = New-Object System.Windows.Forms.Form
+$Form.Text = $WinTitle
+$Form.Size = "410, 280"
+$Form.StartPosition = "CenterScreen"
+$Form.MaximizeBox = $false    #最大化ボタン
+$Form.MinimizeBox = $false    #最小化ボタン
+
+#---+---+---+---+---+---+---+---+---+---+ COMポート1設定
+$X=10; $Y=10
+#ラベル
+$Label_Serial = New-Object System.Windows.Forms.Label
+$Label_Serial.Location = "$X, $Y"
+$Label_Serial.AutoSize = $true   #文字の長さに合わせ自動調整
+$Label_Serial.Text = "シリアル通信設定 #1"
+$Form.Controls.Add($Label_Serial)
+
+# コンボボックス COM番号
+$Combo_Com = New-Object System.Windows.Forms.Combobox
+$Combo_Com.Location = "$X, $($Y + 20)"
+$Combo_Com.size = "60, 30"
+$Combo_Com.DropDownStyle = "DropDown"    #DropDown、DropDownList、Simleから選択し変更可能
+<#
+# コンボボックスに項目を追加
+[void] $Combo_Com.Items.Add("COM1")
+[void] $Combo_Com.Items.Add("COM2")
+[void] $Combo_Com.Items.Add("COM3")
+#$Combo_Com.SelectedIndex = 0
+#>
+$Form.Controls.Add($Combo_Com)
+#ドロップダウン時イベント
+$Combo_Com.Add_DropDown({
+    $Combo_Com.Items.Clear()
+    $ComAry = [System.IO.Ports.SerialPort]::GetPortNames()  #COMポート一覧取得
+    If ($ComAry.Count -gt 0) {
+        [void] $Combo_Com.Items.AddRange($ComAry)  #アイテム追加
+    }
+})
+#結果取得
+#$Result = $Combo_Com.Text    #コンボボックス選択項目取得
+
+# コンボボックス ボーレート
+$Combo_Baud = New-Object System.Windows.Forms.Combobox
+$Combo_Baud.Location = "$($X + 70), $($Y + 20)"
+$Combo_Baud.size = "60, 30"
+$Combo_Baud.DropDownStyle = "DropDown"    #DropDown、DropDownList、Simleから選択し変更可能
+# コンボボックスに項目を追加
+[void] $Combo_Baud.Items.Add("9600")
+[void] $Combo_Baud.Items.Add("19200")
+[void] $Combo_Baud.Items.Add("38400")
+[void] $Combo_Baud.Items.Add("57600")
+[void] $Combo_Baud.Items.Add("115200")
+$Combo_Baud.SelectedIndex = 2
+$Form.Controls.Add($Combo_Baud) 
+#結果取得
+#$Result = $Combo_Baud.Text    #コンボボックス選択項目取得
+
+# コンボボックス パリティ
+$Combo_Parity = New-Object System.Windows.Forms.Combobox
+$Combo_Parity.Location = "$($X + 140), $($Y + 20)"
+$Combo_Parity.size = "60, 30"
+$Combo_Parity.DropDownStyle = "DropDown"    #DropDown、DropDownList、Simleから選択し変更可能
+# コンボボックスに項目を追加
+[void] $Combo_Parity.Items.Add("None")
+[void] $Combo_Parity.Items.Add("Odd")
+[void] $Combo_Parity.Items.Add("Even")
+[void] $Combo_Parity.Items.Add("Mark")
+[void] $Combo_Parity.Items.Add("Space")
+$Combo_Parity.SelectedIndex = 0
+$Form.Controls.Add($Combo_Parity) 
+#結果取得
+#$Result = $Combo_Parity.Text    #コンボボックス選択項目取得
+
+# コンボボックス データ長
+$Combo_Bit = New-Object System.Windows.Forms.Combobox
+$Combo_Bit.Location = "$($X + 210), $($Y + 20)"
+$Combo_Bit.size = "60, 30"
+$Combo_Bit.DropDownStyle = "DropDown"    #DropDown、DropDownList、Simleから選択し変更可能
+# コンボボックスに項目を追加
+[void] $Combo_Bit.Items.Add("8")
+[void] $Combo_Bit.Items.Add("7")
+[void] $Combo_Bit.Items.Add("6")
+[void] $Combo_Bit.Items.Add("5")
+$Combo_Bit.SelectedIndex = 0
+$Form.Controls.Add($Combo_Bit) 
+#結果取得
+#$Result = $Combo_Bit.Text    #コンボボックス選択項目取得
+
+# コンボボックス ストップビット
+$Combo_Stop = New-Object System.Windows.Forms.Combobox
+$Combo_Stop.Location = "$($X + 280), $($Y + 20)"
+$Combo_Stop.size = "90, 30"
+$Combo_Stop.DropDownStyle = "DropDown"    #DropDown、DropDownList、Simleから選択し変更可能
+# コンボボックスに項目を追加
+[void] $Combo_Stop.Items.Add("One")
+[void] $Combo_Stop.Items.Add("OnePointFive")
+[void] $Combo_Stop.Items.Add("Two")
+$Combo_Stop.SelectedIndex = 0
+$Form.Controls.Add($Combo_Stop) 
+#結果取得
+#$Result = $Combo_Stop.Text    #コンボボックス選択項目取得
+
+#---+---+---+---+---+---+---+---+---+---+ COMポート2設定
+$X=10; $Y=60
+#ラベル
+$Label_Serial2 = New-Object System.Windows.Forms.Label
+$Label_Serial2.Location = "$X, $Y"
+$Label_Serial2.AutoSize = $true            #文字の長さに合わせ自動調整
+$Label_Serial2.Text = "シリアル通信設定 #2"
+$Form.Controls.Add($Label_Serial2)
+
+# コンボボックス COM番号
+$Combo_Com2 = New-Object System.Windows.Forms.Combobox
+$Combo_Com2.Location = "$X, $($Y + 20)"
+$Combo_Com2.size = "60, 30"
+$Combo_Com2.DropDownStyle = "DropDown"    #DropDown、DropDownList、Simleから選択し変更可能
+<#
+# コンボボックスに項目を追加
+[void] $Combo_Com2.Items.Add("COM1")
+[void] $Combo_Com2.Items.Add("COM2")
+[void] $Combo_Com2.Items.Add("COM3")
+#$Combo_Com2.SelectedIndex = 0
+#>
+$Form.Controls.Add($Combo_Com2) 
+#ドロップダウン時イベント
+$Combo_Com2.Add_DropDown({
+    $Combo_Com2.Items.Clear()
+    $ComAry = [System.IO.Ports.SerialPort]::GetPortNames()  #COMポート一覧取得
+    If ($ComAry.Count -gt 0) {
+        [void] $Combo_Com2.Items.AddRange($ComAry)  #アイテム追加
+    }
+})
+#結果取得
+#$Result = $Combo_Com2.Text    #コンボボックス選択項目取得
+
+# コンボボックス ボーレート
+$Combo_Baud2 = New-Object System.Windows.Forms.Combobox
+$Combo_Baud2.Location = "$($X + 70), $($Y + 20)"
+$Combo_Baud2.size = "60, 30"
+$Combo_Baud2.DropDownStyle = "DropDown"    #DropDown、DropDownList、Simleから選択し変更可能
+# コンボボックスに項目を追加
+[void] $Combo_Baud2.Items.Add("9600")
+[void] $Combo_Baud2.Items.Add("19200")
+[void] $Combo_Baud2.Items.Add("38400")
+[void] $Combo_Baud2.Items.Add("57600")
+[void] $Combo_Baud2.Items.Add("115200")
+$Combo_Baud2.SelectedIndex = 2
+$Form.Controls.Add($Combo_Baud2) 
+#結果取得
+#$Result = $Combo_Baud2.Text    #コンボボックス選択項目取得
+
+# コンボボックス パリティ
+$Combo_Parity2 = New-Object System.Windows.Forms.Combobox
+$Combo_Parity2.Location = "$($X + 140), $($Y + 20)"
+$Combo_Parity2.size = "60, 30"
+$Combo_Parity2.DropDownStyle = "DropDown"    #DropDown、DropDownList、Simleから選択し変更可能
+# コンボボックスに項目を追加
+[void] $Combo_Parity2.Items.Add("None")
+[void] $Combo_Parity2.Items.Add("Odd")
+[void] $Combo_Parity2.Items.Add("Even")
+[void] $Combo_Parity2.Items.Add("Mark")
+[void] $Combo_Parity2.Items.Add("Space")
+$Combo_Parity2.SelectedIndex = 0
+$Form.Controls.Add($Combo_Parity2) 
+#結果取得
+#$Result = $Combo_Parity2.Text    #コンボボックス選択項目取得
+
+# コンボボックス データ長
+$Combo_Bit2 = New-Object System.Windows.Forms.Combobox
+$Combo_Bit2.Location = "$($X + 210), $($Y + 20)"
+$Combo_Bit2.size = "60, 30"
+$Combo_Bit2.DropDownStyle = "DropDown"    #DropDown、DropDownList、Simleから選択し変更可能
+# コンボボックスに項目を追加
+[void] $Combo_Bit2.Items.Add("8")
+[void] $Combo_Bit2.Items.Add("7")
+[void] $Combo_Bit2.Items.Add("6")
+[void] $Combo_Bit2.Items.Add("5")
+$Combo_Bit2.SelectedIndex = 0
+$Form.Controls.Add($Combo_Bit2) 
+#結果取得
+#$Result = $Combo_Bit2.Text    #コンボボックス選択項目取得
+
+# コンボボックス ストップビット
+$Combo_Stop2 = New-Object System.Windows.Forms.Combobox
+$Combo_Stop2.Location = "$($X + 280), $($Y + 20)"
+$Combo_Stop2.size = "90, 30"
+$Combo_Stop2.DropDownStyle = "DropDown"    #DropDown、DropDownList、Simleから選択し変更可能
+# コンボボックスに項目を追加
+[void] $Combo_Stop2.Items.Add("One")
+[void] $Combo_Stop2.Items.Add("OnePointFive")
+[void] $Combo_Stop2.Items.Add("Two")
+$Combo_Stop2.SelectedIndex = 0
+$Form.Controls.Add($Combo_Stop2) 
+#結果取得
+#$Result = $Combo_Stop2.Text    #コンボボックス選択項目取得
+
+#---+---+---+---+---+---+---+---+---+---+ バイナリ表示
+$X=10; $Y=110  #表示位置
+# チェックボックス
+$CheckBox_Bin = New-Object System.Windows.Forms.CheckBox
+$CheckBox_Bin.Location = "$X, $Y"
+$CheckBox_Bin.Size = "100, 30"
+$CheckBox_Bin.Text = "バイナリ表示"
+$Form.Controls.Add($CheckBox_Bin)
+#結果取得
+#$Result = $CheckBox_Bin.Checked    #チェックボックス取得 $true $false
+
+#---+---+---+---+---+---+---+---+---+---+ ログ出力
+$X=120; $Y=110  #表示位置
+# チェックボックス
+$CheckBox_Log = New-Object System.Windows.Forms.CheckBox
+$CheckBox_Log.Location = "$X, $Y"
+$CheckBox_Log.Size = "100, 30"
+$CheckBox_Log.Text = "ログ出力"
+$Form.Controls.Add($CheckBox_Log)
+#結果取得
+#$Result = $CheckBox_Log.Checked    #チェックボックス取得 $true $false
+
+#---+---+---+---+---+---+---+---+---+---+
+$X=10; $Y=150
+#ラベル
+$Label = New-Object System.Windows.Forms.Label
+$Label.Location = "$X, $Y"
+$Label.AutoSize = $True            #文字の長さに合わせ自動調整
+$Label.Text = '起動オプション'
+$Form.Controls.Add($Label)
+
+#テキストボックス 起動オプション
+$TextBox_Arg = New-Object System.Windows.Forms.TextBox
+$TextBox_Arg.Location = "$X, $($Y + 20)"
+#$TextBox_Arg.MaxLength = 128       #最大入力文字数
+$TextBox_Arg.ReadOnly = $true      #編集不可
+$TextBox_Arg.Anchor = (([System.Windows.Forms.AnchorStyles]::Left)`
+              -bor ([System.Windows.Forms.AnchorStyles]::Top)`
+              -bor ([System.Windows.Forms.AnchorStyles]::Right)`
+              -bor ([System.Windows.Forms.AnchorStyles]::Bottom))    #位置固定(画面サイズ変更時など)
+$TextBox_Arg.Size = "365, 10"
+$Form.Controls.Add($TextBox_Arg)
+
+#ボタン OK
+$OkButton = New-Object System.Windows.Forms.Button
+$OkButton.Location = "$($X + 280), $($Y + 50)"
+$OkButton.Size = "70, 23"
+$OkButton.Text = '実行'
+$OkButton.Anchor = (([System.Windows.Forms.AnchorStyles]::Right)`
+               -bor ([System.Windows.Forms.AnchorStyles]::Bottom))    #位置固定(画面サイズ変更時など)
+$OkButton.DialogResult = [System.Windows.Forms.DialogResult]::OK
+$Form.AcceptButton = $OkButton
+$Form.Controls.Add($OkButton)
+
+#ボタン キャンセル
+$CancelButton = New-Object System.Windows.Forms.Button
+$CancelButton.Location = "$($X + 470), $($Y + 50)"
+$CancelButton.Size = "75, 23"
+$CancelButton.Text = 'Cancel'
+$CancelButton.Anchor = (([System.Windows.Forms.AnchorStyles]::Right)`
+                   -bor ([System.Windows.Forms.AnchorStyles]::Bottom))    #位置固定(画面サイズ変更時など)
+$CancelButton.DialogResult = [System.Windows.Forms.DialogResult]::Cancel
+$Form.CancelButton = $CancelButton
+$Form.Controls.Add($CancelButton)
+
+################################################## メイン処理
+#フォーム表示
+$Form.Topmost = $true    #フォームを常に手前に表示
+#$Form.Add_Shown({$TextBox_Arg.Select()})    #フォームをアクティブにし、テキストボックスにフォーカスを設定
+
+$Timer.Start()    #タイマー起動
+$result = $Form.ShowDialog()    #フォーム表示
+$Timer.Stop()    #タイマー終了
+
+If ($result -eq [System.Windows.Forms.DialogResult]::OK) {
+    If ($TextBox_Arg.Text -ne "") {
+        Start-Process -FilePath $ScrName -ArgumentList $TextBox_Arg.Text    #外部プログラムexe起動
+        #PowerShellで起動時は下記にて行う
+        #$ArgStr = "-ExecutionPolicy RemoteSigned -command " + $ScrName + $TextBox_Arg.Text  #-WindowStyle Hidden 
+        #Start-Process -FilePath powershell.exe -ArgumentList $ArgStr -Wait    #外部プログラムps1起動、終了待ち
+    } Else {
+        Start-Process -FilePath $ScrName    #パラメータ指定無いときは、そのまま外部プログラム起動
+    }
+}
+
+################################################## 終了処理
+Exit    #スクリプト終了
+
+################################################## End Of File
